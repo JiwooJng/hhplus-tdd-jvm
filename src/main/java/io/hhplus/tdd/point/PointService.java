34b422eb166f5bd1ea23e7a18d7953cadf551ef5
JiwooJng <jjjw1225@gmail.com>
2025-03-27 22:54:57 +0900

    PointService 예외 상황 추가 구현 1. 충전 가능 금액 > 0 2. 충전 최대 한도 <= 1,000,000 3. 포인트 사용 가능 금액 > 0 4. 포인트 사용 금액 >= 잔액

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -19,13 +19,15 @@ public class PointService {
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
+    private static final long MAX_POINT = 1_000_000;
+
     public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
         this.userPointTable = userPointTable;
         this.pointHistoryTable = pointHistoryTable;
     }
 
     public UserPoint getUserPoint(@PathVariable long userId) {
-        UserPoint userPoint = this.userPointTable.selectById(userId);
+        UserPoint userPoint = userPointTable.selectById(userId);
 
         log.info("유저 포인트 조회 > 유저 id: {}, 잔액: {}", userId, userPoint.point());
 
@@ -33,21 +35,31 @@ public class PointService {
     }
 
     public List<PointHistory> getPointHistoryList(@PathVariable long userId) {
-         List<PointHistory> pointHistory = this.pointHistoryTable.selectAllByUserId(userId);
+         List<PointHistory> pointHistory = pointHistoryTable.selectAllByUserId(userId);
 
-         log.info("유저 포인트 내역 조회 > 유저 id: {}%n 포인트 내역: {}", userId, pointHistory);
+         log.info("유저 포인트 내역 조회 > 유저 id: {} 포인트 내역: {}", userId, pointHistory);
 
          return pointHistory;
     }
 
     public UserPoint charge(@PathVariable long userId, @RequestBody long chargeAmount) {
-        UserPoint userPoint = this.userPointTable.selectById(userId);
+        if (chargeAmount <= 0) {
+            log.error("포인트 충전 금액은 1원 이상이어야 합니다.");
+            throw new IllegalArgumentException("포인트 충전 실패");
+        }
+
+        UserPoint userPoint = userPointTable.selectById(userId);
         log.info("포인트 충전 | 유저 조회 > id: {}, 잔액: {}", userId, userPoint.point());
 
         log.info("포인트 충전 금액: {}", chargeAmount);
         long wholeAmount = userPoint.point() + chargeAmount;
 
-        UserPoint updateUserPoint = this.userPointTable.insertOrUpdate(userId, wholeAmount);
+        if (wholeAmount > MAX_POINT) {
+            log.error("포인트 충전 최대 한도는 1,000,000(원)입니다.");
+            throw new IllegalArgumentException("포인트 충전 실패");
+        }
+
+        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, wholeAmount);
         pointHistoryTable.insert(userId, wholeAmount, TransactionType.CHARGE, System.currentTimeMillis());
 
         log.info("포인트 충전 완료 > 잔액: {}", wholeAmount);
@@ -55,13 +67,23 @@ public class PointService {
     }
 
     public UserPoint use(@PathVariable long userId, @RequestBody long useAmount) {
-        UserPoint userPoint = this.userPointTable.selectById(userId);
+        if (useAmount <= 0) {
+            log.error("포인트 사용 금액은 1원 이상이어야 합니다.");
+            throw new IllegalArgumentException("포인트 사용 실패");
+        }
+
+        UserPoint userPoint = userPointTable.selectById(userId);
         log.info("포인트 사용 | 유저 조회 > id: {}, 잔액: {}", userId, userPoint.point());
 
         log.info("포인트 사용 금액: {}", useAmount);
         long remainPoint = userPoint.point() - useAmount;
 
-        UserPoint updateUserPoint = this.userPointTable.insertOrUpdate(userId, remainPoint);
+        if (remainPoint < 0) {
+            log.error("포인트 잔액이 부족합니다.");
+            throw new IllegalArgumentException("포인트 사용 실패");
+        }
+
+        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userId, remainPoint);
         pointHistoryTable.insert(userId, remainPoint, TransactionType.USE, System.currentTimeMillis());
 
         log.info("포인트 사용 성공 > 잔액: {}", remainPoint);
